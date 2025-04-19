282dd82b62efeecccf92128f4f6027c6dc09bb8f
Guillaume Belz <gbelz@esri.com>
2025-04-18 18:03:43 -0700

    remove QRT_DISABLE_DEPRECATED_WARNINGS

--- uitools/common/src/PopupViewController.cpp
+++ uitools/common/src/PopupViewController.cpp
@@ -14,7 +14,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  ******************************************************************************/
-#define QRT_DISABLE_DEPRECATED_WARNINGS
 
 #include "PopupViewController.h"
 
